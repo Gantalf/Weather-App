c26b98a093da91c61f5d1d986f9a37b67a921abe
Luciano <pereiraluciano169@gmail.com>
2021-08-08 16:54:04 -0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>Hola, Soy Luciano.</h1>
+            <p>Esta aplicacion esta hecha para quepuedas
+            ver la condicion climatica de todas las ciudades del mundo! :)
+            </p>
+        </div>
+    )
+}
